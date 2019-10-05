73f889b8c2544f1805d2f072f629c769e6ddc8bb
Trevor Fisher <minalear@gmail.com>
2019-10-05 11:19:52 -0500

    Initial project commit.

---
+++ android/app/src/main/java/edu/uco/tfisher9/quizme/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.uco.tfisher9.quizme;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
