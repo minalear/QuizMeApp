f24459de2a8b20cfcf7b32eb2d730bfbba488e0e
Trevor Fisher <minalear@gmail.com>
2019-10-05 11:36:03 -0500

    Added frontpage and setup basic app.

---
+++ lib/view/frontpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FrontPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QuizMe'),
+      ),
+      body: Text('Body'),
+    );
+  }
+}
