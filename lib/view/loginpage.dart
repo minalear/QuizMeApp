4e82728bb120f64b38e59afb878e22a16ca19494
Trevor Fisher <minalear@gmail.com>
2019-10-29 11:22:51 -0500

    Fixed overflow error on LoginPage.

--- lib/view/loginpage.dart
+++ lib/view/loginpage.dart
@@ -24,72 +24,75 @@ class LoginPageState extends State<LoginPage> {
     return WillPopScope(
       onWillPop: (){ return Future.value(false); },
       child: Scaffold(
-        resizeToAvoidBottomInset: false,
         body: Form(
           key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(left: 15, right: 15),
-                child: Image.asset('assets/quizme-logo-banner.png')
-              ),
-              Container(
-                padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
-                child: TextFormField(
-                  initialValue: '',
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    hintText: 'email',
-                    labelText: 'email',
-                    icon: Icon(Icons.email),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 15, right: 15),
+                    child: Image.asset('assets/quizme-logo-banner.png')
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: Validator.validateEmail,
-                  onSaved: controller.saveEmail,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(bottom: 25, left: 10, right: 10),
-                child: TextFormField(
-                  initialValue: '',
-                  autocorrect: false,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'password',
-                    labelText: 'password',
-                    icon: Icon(Icons.lock),
-                  ),
-                  keyboardType: TextInputType.text,
-                  validator: Validator.validatePassword,
-                  onSaved: controller.savePassword,
-                ),
-              ),
-              SizedBox(
-                child: RaisedButton(
-                    child: Text('Sign In'),
-                    onPressed: (){ controller.login(); },
-                  ),
-                  width: double.infinity,
-                  height: 45,
-              ),
-              // InkWell allows us to create clickable text
-              InkWell(
-                // RichText widget allows us to style text in creative ways
-                onTap: (){ controller.createAccount(); },
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(text: 'Don\'t have an account? '),
-                        TextSpan(text: 'Signup here!', style: TextStyle(color: Theme.of(context).hintColor)),
-                      ],
+                  Container(
+                    padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+                    child: TextFormField(
+                      initialValue: '',
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        hintText: 'email',
+                        labelText: 'email',
+                        icon: Icon(Icons.email),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: Validator.validateEmail,
+                      onSaved: controller.saveEmail,
                     ),
                   ),
-                ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 25, left: 10, right: 10),
+                    child: TextFormField(
+                      initialValue: '',
+                      autocorrect: false,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'password',
+                        labelText: 'password',
+                        icon: Icon(Icons.lock),
+                      ),
+                      keyboardType: TextInputType.text,
+                      validator: Validator.validatePassword,
+                      onSaved: controller.savePassword,
+                    ),
+                  ),
+                  SizedBox(
+                    child: RaisedButton(
+                        child: Text('Sign In'),
+                        onPressed: (){ controller.login(); },
+                      ),
+                      width: double.infinity,
+                      height: 45,
+                  ),
+                  // InkWell allows us to create clickable text
+                  InkWell(
+                    // RichText widget allows us to style text in creative ways
+                    onTap: (){ controller.createAccount(); },
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(text: 'Don\'t have an account? '),
+                            TextSpan(text: 'Signup here!', style: TextStyle(color: Theme.of(context).hintColor)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
