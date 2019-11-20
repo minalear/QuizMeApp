8610fead6d8e53be243207d669cbb34b006a65b8
Trevor Fisher <minalear@gmail.com>
2019-11-20 15:24:16 -0600

    Implementing question types.

--- lib/model/question.dart
+++ lib/model/question.dart
@@ -1,9 +1,37 @@
+enum QuestionTypes { Normal, Image, Boolean }
+
 class Question {
+  QuestionTypes type;
+
+  // The correct answer will be set as the first possible answer
+  /*String get correctAnswer { 
+    return answers[0];
+  }*/
+}
+
+class NormalQuestion extends Question {
   String question;
   List<String> answers;
 
-  // The correct answer will be set as the first possible answer
-  String get correctAnswer { 
-    return answers[0];
+  NormalQuestion() {
+    type = QuestionTypes.Normal;
+  }
+}
+
+class ImageQuestion extends Question {
+  String question, imageUri;
+  List<String> answers;
+
+  ImageQuestion() {
+    type = QuestionTypes.Image;
+  }
+}
+
+class BooleanQuestion extends Question {
+  String question;
+  bool correctAnswer; // might make string?
+
+  BooleanQuestion() {
+    type = QuestionTypes.Boolean;
   }
 }
